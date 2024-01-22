c2047c54cb5d028c3d458b40b493d3faa3a27ccf
Chiransh Somani <chiru1904@gmail.com>
2024-01-22 21:01:52 +0530

    Chiransh Somani

---
+++ Chiransh_22b1202/Assignment3/script.js
@@ -0,0 +1,11 @@
+function submitDetails() {
+   var name = document.getElementById("name").value;
+   var phoneNumber = document.getElementById("phoneNumber").value;
+   var email = document.getElementById("email").value;
+
+   console.log("Name: " + name);
+   console.log("Phone Number: " + phoneNumber);
+   console.log("Email: " + email);
+
+   alert("Check console log!");
+}
